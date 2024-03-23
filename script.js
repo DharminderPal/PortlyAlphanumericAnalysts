cd81c03243fa0554b7714c72043bf136bdb6cf73
DharminderPal <144053986+DharminderPal@users.noreply.github.com>
2024-03-23 07:45:36 +0000

    this is pattern 2

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
+
+
+// 11111
+// 246810
+// 3691215
+// 48121620
+// 510152025  
+// *****************  1st this is the answer *****************
+// let n = 5;
+// let str = "";
+// let count = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     if (i == 1) {
+//       str += `${i}`
+//     }
+//     else (
+//       str += `${i * j}`
+//     )
+//   }
+//   str += "\n";
+// }
+// console.log(str);
+
+
+
+// *****************  2nd this is the answer *****************
+
 let n = 5;
 let str = "";
 let count = 1;
 
 for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= n; j++) {
-    if (i == 1) {
-      str += `${i}`
+  for (let j = 1; j <= i; j++) {
+    if ((i + j) % 2 == 0) {
+      str += `${" 1"}`
+
     }
+
     else (
-      str += `${i * j}`
+      str += `${" 0"}`
     )
   }
   str += "\n";
 }
 console.log(str);
+
+
