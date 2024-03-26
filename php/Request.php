74b1448f8f249c5b15fa7f6439ae26cf9c74c39c
DharminderPal <144053986+DharminderPal@users.noreply.github.com>
2024-03-26 11:45:04 +0000

    php request method

---
+++ php/Request.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+   <form action="Request1.php"  method="get">
+    <!-- request method mai POST and get dono kaam krnege let go -->
+   Name:<input type="text" name="fname"><br><br>
+
+   Age:<input type="text" name="age"><br><br>
+
+   password: <input type="password" ><br><br>
+
+<input type="submit" name="save">
+<input type="reset" >
+
+
+
+   </form> 
+</body>
+</html>
