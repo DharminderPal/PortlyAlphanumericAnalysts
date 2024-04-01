c18583a1fe6cc6877bb211629b4077f1584190ea
dharminder <144053986+DharminderPal@users.noreply.github.com>
2024-04-01 12:43:32 +0000

    project not complete

---
+++ php base project/inser_data.php
@@ -0,0 +1,17 @@
+<?php
+
+if(isset($_POST['add_students'])){
+
+$f_name=$_post['f_name'];
+$last_name=$_post['last_name'];  
+$age=$_post['age'];
+}
+
+
+if( $fname ==""||($empty))
+{
+    header('location:proejct.php?m=you need to fill in the first name');
+}
+
+
+?>
